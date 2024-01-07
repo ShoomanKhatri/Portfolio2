554957e04a7d3dbba92a8986b3bcfb440330d294
Shoomankhatri <shoomankhatri@gmail.com>
2024-01-07 21:26:32 +0545

    done

--- src/Components/Works/works.jsx
+++ src/Components/Works/works.jsx
@@ -4,11 +4,12 @@ import UIdesign from "../../assets/ui-design.png";
 import Currencyconverter from "../../assets/currencyconverter.png";
 import temperatureconverter from "../../assets/temperatureconverter.png";
 import productlandingpage from "../../assets/productlandingpage.png";
+import productlandingpage2 from "../../assets/product-landing-page2.png";
 
 const Portfolio = () => {
     return (
         <section id='portfolio'>
-            
+
             <h5>My Recent Work</h5>
             <h2>Projects</h2>
 
@@ -16,7 +17,7 @@ const Portfolio = () => {
 
                 <article className='portfolio_item'>
                     <div className="portfolio_item-image">
-                        <img src={Currencyconverter} alt="Currencyconverter"  className='project_img'/>
+                        <img src={Currencyconverter} alt="Currencyconverter" className='project_img' />
                     </div>
                     <h3>Currency Converter</h3>
                     <button className='btnTools'>React</button>
@@ -32,7 +33,7 @@ const Portfolio = () => {
                         <img src={temperatureconverter} alt="" className='project_img' />
                     </div>
                     <h3>Temperature Converter</h3>
-                    <button className='btnTools'>React</button> 
+                    <button className='btnTools'>React</button>
                     <button className='btnTools'>Web App</button>
                     <button className='btnTools'>Tailwind CSS</button> <br />
                     <button className='btn1'>
@@ -42,7 +43,7 @@ const Portfolio = () => {
 
                 <article className='portfolio_item'>
                     <div className="portfolio_item-image">
-                        <img src={productlandingpage} alt="" className='project_img'/>
+                        <img src={productlandingpage} alt="" className='project_img' />
                     </div>
                     <h3>Product Landing Page</h3>
                     <button className='btnTools'>React</button>
@@ -54,17 +55,17 @@ const Portfolio = () => {
 
                 <article className='portfolio_item'>
                     <div className="portfolio_item-image">
-                        <img src={UIdesign} alt="" className='project_img'/>
+                        <img src={productlandingpage2} alt="" className='project_img' />
                     </div>
-                    <h3>Pending.</h3>
+                    <h3>Product Landing Page 2</h3>
                     <button className='btn1'>
-                        <a href="https://www.youtube.com" target='_blank'>Live Demo</a>
+                        <a href="https://product-landing-page2.vercel.app/" target='_blank'>Live Demo</a>
                     </button>
                 </article>
 
                 <article className='portfolio_item'>
                     <div className="portfolio_item-image">
-                        <img src={UIdesign} alt="" className='project_img'/>
+                        <img src={UIdesign} alt="" className='project_img' />
                     </div>
                     <h3>Pending.</h3>
                     <button className='btn1'>
