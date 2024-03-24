8d290d8dfa196a9a894dac4901e6726f84ad5e3f
Shooman Khatri <112597601+ShoomanKhatri@users.noreply.github.com>
2024-03-24 14:59:44 +0545

    updated

--- src/Components/Works/works.jsx
+++ src/Components/Works/works.jsx
@@ -8,6 +8,7 @@ import productlandingpage2 from "../../assets/product-landing-page2.png";
 import TodoList from "../../assets/TodoListNext.png";
 import cottoncanva from "../../assets/cottoncanva.jpg";
 import cottoncanva1 from "../../assets/cottoncanva1.png";
+import Restro from "../../assets/Restro.png";
 
 const Portfolio = () => {
     return (
@@ -17,6 +18,20 @@ const Portfolio = () => {
             <h2>Projects</h2>
 
             <div className="portfolio_container">
+
+
+                <article className='portfolio_item'>
+                    <div className="portfolio_item-image">
+                        <img src={Restro} alt="Currencyconverter" className='project_img' />
+                    </div>
+                    <h3>Restaurant App</h3>
+                    <button className='btnTools'>MERN</button>
+                    <button className='btnTools'>Vercel</button>
+                    <button className='btnTools'>Render</button> <br />
+                    <button className='btn1'>
+                        <a href="https://restaurant-app-olive.vercel.app/" target='_blank'>Live Demo</a>
+                    </button>
+                </article>
 
                 <article className='portfolio_item'>
                     <div className="portfolio_item-image">
@@ -88,6 +103,7 @@ const Portfolio = () => {
                     </div>
                     <h3>cottoncanva</h3>
                     <button className='btnTools'>Wordpress</button>
+                    <button className='btnTools'>Ecommerce App</button>
                     <button className='btnTools'>CMS</button> <br />
                     <button className='btn1'>
                         <a href="https://dev-cottoncanva.pantheonsite.io/" target='_blank'>Live Demo</a>
